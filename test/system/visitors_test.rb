daadc946d4143e00f736b2d56fd780e5dd4cd736
Suraj Vadgama <suninthesky@users.noreply.github.com>
2023-12-12 09:43:41 +0000

    Change button copy

--- test/system/visitors_test.rb
+++ test/system/visitors_test.rb
@@ -7,7 +7,7 @@ class BookingsTest < ApplicationSystemTestCase
   end
 
   test 'no postcode' do
-    assert_link 'Book', count: 2
+    assert_link 'View', count: 2
   end
 
   test 'valid postcode' do
@@ -17,7 +17,7 @@ class BookingsTest < ApplicationSystemTestCase
 
     fill_in 'Postcode', with: @host.postcode
     click_on 'Filter'
-    assert_link 'Book', count: 2
+    assert_link 'View', count: 2
   end
 
   test 'invalid postcode' do
@@ -29,13 +29,13 @@ class BookingsTest < ApplicationSystemTestCase
   test 'filter by heat pump type' do
     select @host.hp_type, from: :q_hp_type_in
     click_on 'Filter'
-    assert_link 'Book', count: 1
+    assert_link 'View', count: 1
   end
 
   test 'filter by property type' do
     select @host.property_type, from: :q_property_type_in
     click_on 'Filter'
-    assert_link 'Book', count: 1
+    assert_link 'View', count: 1
   end
 
   test 'can follow host' do
